297e6b7df606f4d1160ab90cf9c932cecf3db0c2
MichaelAChrisco <michaelachrisco@gmail.com>
2017-12-06 21:11:10 -0800

    Init 834 segments

---
+++ Hipaa/Rules/EF_HIPAA_005010X222A1_834.cs
@@ -0,0 +1,24 @@
+﻿namespace EdiFabric.Sdk.Hipaa.Rules
+{
+    using System;
+    using System.Collections.Generic;
+    using EdiFabric.Core.Annotations.Edi;
+    using EdiFabric.Core.Annotations.Validation;
+    using EdiFabric.Core.Model.Edi;
+    using EdiFabric.Core.Model.Edi.X12;
+    using EdiFabric.Core.Model.Edi.ErrorContexts;
+
+
+    [Serializable()]
+    [Message("X12", "005010X222A1", "834")]
+    public class TS834 : EdiMessage
+    {
+
+        [Pos(1)]
+        public ST ST { get; set; }
+        [Required]
+        [Pos(2)]
+        public SE SE { get; set; }
+
+    }
+}
