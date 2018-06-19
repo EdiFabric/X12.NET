b5c9aa55ec09c202688ab335b8dbee4ea8123988
Kamen Nikolov <azagal@gmail.com>
2018-06-19 17:42:13 +0100

    Added custom templates for X12

--- EdiFabric.Sdk.X12.Templates.V004010.Custom1/EF_X12_004010_TS850.cs
+++ EdiFabric.Sdk.X12.Templates.V004010.Custom1/EF_X12_004010_TS850.cs
@@ -1,18 +1,34 @@
 namespace EdiFabric.Rules.X12_004010
 {
     using System;
-    using System.Collections.Generic;
     using EdiFabric.Core.Annotations.Edi;
     using EdiFabric.Core.Annotations.Validation;
 
+    /// <summary>
+    /// Custom version of the purchase order template for trading partner with id CUSTOM1
+    /// </summary>
     [Serializable()]
     [Message("X12", "004010", "850")]
     public class TS850Custom1 : TS850
     {
-        [Splitter]
         [Required]
-        [ListCount(100000)]
-        [Pos(38)]
-        public new List<TS850_PO1Loop1> PO1Loop1 { get; set; }       
+        [Pos(2)]
+        public new BEGCustom1 BEG { get; set; }
+    }
+
+    [Serializable()]
+    [Segment("BEG")]
+    public class BEGCustom1 : BEG
+    {
+        //  Mark as mandatory
+        [Required]
+        [StringLength(1, 30)]
+        [DataElement("328", typeof(X12_AN))]
+        [Pos(4)]
+        public new string ReleaseNumber_04 { get; set; }
+
+        //  Add new field
+        [Pos(13)]
+        public string NewField_13 { get; set; }
     }
 }
--- EdiFabric.Sdk.X12.Templates.V004010.Custom2/EF_X12_004010_TS850.cs
+++ EdiFabric.Sdk.X12.Templates.V004010.Custom2/EF_X12_004010_TS850.cs
@@ -5,14 +5,47 @@ namespace EdiFabric.Rules.X12_004010
     using EdiFabric.Core.Annotations.Edi;
     using EdiFabric.Core.Annotations.Validation;
 
+    /// <summary>
+    /// Custom version of the purchase order template for trading partner with id CUSTOM2
+    /// </summary>
     [Serializable()]
     [Message("X12", "004010", "850")]
-    public class TS850Custom2 : TS850
+    public class TS850Custom2 : TS850Custom1
     {
-        [Splitter]
+        [ListCount(200)]
+        [Pos(34)]
+        public new List<TS850_N1Loop1Inherited1> N1Loop1 { get; set; }
+    }
+
+    [Serializable()]
+    [Group(typeof(N1))]
+    public class TS850_N1Loop1Inherited1 : TS850_N1Loop1
+    {
+        //  Swap the positions of N2 and N3
+        [ListCount(2)]
+        [Pos(3)]
+        public new List<N2Inherited2> N2 { get; set; }
+
+        [ListCount(2)]
+        [Pos(2)]
+        public new List<N3> N3 { get; set; }
+    }
+
+    [Serializable()]
+    [Segment("N2")]
+    public class N2Inherited2 : N2
+    {
+        //  Change the validation attribute and data element type at position 1
         [Required]
-        [ListCount(100000)]
-        [Pos(38)]
-        public new List<TS850_PO1Loop1> PO1Loop1 { get; set; }       
+        [StringLength(1, 60)]
+        [DataElement("93", typeof(X12_ID_706))]
+        [Pos(1)]
+        public new string Name_01 { get; set; }
+
+        //  Add extra field
+        [StringLength(1, 60)]
+        [DataElement("94", typeof(X12_AN))]
+        [Pos(3)]
+        public string ExtraName_03 { get; set; }
     }
 }
